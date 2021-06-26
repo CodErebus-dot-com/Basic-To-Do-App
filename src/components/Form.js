786a197b37996f1af4bfa693ba8d78c20ebca414
VS <varkisebi@gmail.com>
2021-06-26 20:55:56 +0530

    Add todo validation

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,18 +1,31 @@
 import React from 'react'
+import { v4 as uuidv4 } from 'uuid';
+import { useSnackbar } from 'notistack';
 
 const Form = ({inputText, setInputText, todos, setTodos, setFilterOption}) => {
+    
+    const { enqueueSnackbar } = useSnackbar()
+
     const handleInputText = (e) => {
         setInputText(e.target.value)
     }
 
     const handleSubmitTodos = (e) => {
         e.preventDefault()
-        const todoItems = [...todos, {
-            inputText: inputText,
-            id: Math.random()*1000,
-            completed: false 
-        }]    
-        setTodos(todoItems)
+
+
+        if(inputText.length > 0){
+            const todoItems = [...todos, {
+                inputText: inputText,
+                id: uuidv4(),
+                completed: false 
+            }]
+            setTodos(todoItems)
+        } 
+        else {
+            enqueueSnackbar('Cannot add an empty todo!', { variant: 'error' })
+        }
+
         setInputText("")
     }
 
@@ -30,6 +43,7 @@ const Form = ({inputText, setInputText, todos, setTodos, setFilterOption}) => {
                     type="text" 
                     className="todo-input"
                     placeholder="Add Todo"
+                    required
                 />
                 <button onClick={handleSubmitTodos}>
                     <i className="fas fa-plus-square"></i>
@@ -47,73 +61,3 @@ const Form = ({inputText, setInputText, todos, setTodos, setFilterOption}) => {
 }
 
 export default Form
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React from 'react'
-
-// const Form = ({inputText, setInputText, todos, setTodos, setStatus}) => {
-//     const inputTextHandler = (e) => {
-//         console.log(e.target.value);
-//         setInputText(e.target.value);
-//     }
-
-//     const submitHandler = (e) => {
-//         e.preventDefault()
-//         setTodos([...todos,{
-//             inputText: inputText,
-//             id: Math.random()*1000,
-//             completed: false
-//         }
-//         ])
-//         setInputText("")
-//     }
-
-//     const statusHandler = (e) => {
-//         console.log(e.target.value);
-//         setStatus(e.target.value)
-//     }
-
-//     return(
-//         <div>
-//             <form>
-//                 <input 
-//                     onChange={inputTextHandler} 
-//                     value={inputText} 
-//                     type="text" 
-//                     className="todo-input"
-//                 />
-//                 <button onClick={submitHandler}>
-//                     <i className="fas fa-plus-square"></i>
-//                 </button>
-//                 <div onChange={statusHandler} className="select">
-//                     <select className="filter-todo">
-//                         <option value="all">All</option>
-//                         <option value="completed">Completed</option>
-//                         <option value="uncompleted">Uncompleted</option>
-//                     </select>
-//                 </div>
-//             </form>
-//         </div>
-//     )
-// }
-
-// export default Form
